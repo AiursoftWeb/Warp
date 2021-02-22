4d587a555de0680bf424f2a48e19615369dd2bd6
Anduin Xue <anduin.xue@microsoft.com>
2021-02-22 14:59:59 +0800

    Init project.

---
+++ Data/WrapDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Aiursoft.Wrap.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aiursoft.Wrap.Data
+{
+    public class WrapDbContext : IdentityDbContext<WrapUser>
+    {
+        public WrapDbContext(DbContextOptions<WrapDbContext> options) : base(options)
+        {
+        }
+    }
+}
