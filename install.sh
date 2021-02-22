9263935c90a3d6a2d047a57503e2a6f6b76d1e2c
Anduin Xue <anduin@aiursoft.com>
2021-02-23 01:42:48 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://github.com/AiursoftWeb/AiurScript/raw/master/$1.sh | sudo bash -s $arg; }
+wrap_path="/opt/apps/Wrap"
+
+install_wrap()
+{
+    if [[ $(curl -sL ifconfig.me) == "$(dig +short $1)" ]]; 
+    then
+        echo "IP is correct."
+    else
+        echo "$1 is not your current machine IP!"
+        return 9
+    fi
+
+    port=$(aiur network/get_port) && echo "Using internal port: $port"
+    aiur network/enable_bbr
+    aiur system/set_aspnet_prod
+    aiur install/caddy
+    aiur install/dotnet
+    aiur git/clone_to AiursoftWeb/Wrap ./Wrap
+    aiur dotnet/publish $wrap_path ./Wrap/Wrap.csproj
+    aiur services/register_aspnet_service "wrap" $port $wrap_path "Aiursoft.Wrap"
+    aiur caddy/add_proxy $1 $port
+    aiur firewall/enable_firewall
+    aiur firewall/open_port 443
+    aiur firewall/open_port 80
+
+    echo "Successfully installed Wrap as a service in your machine! Please open https://$1 to try it now!"
+    rm ./Wrap -rf
+}
+
+install_wrap
