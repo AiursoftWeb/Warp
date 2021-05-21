3bbcb1b7e41d6936a8ec18ee837508a665d73545
Anduin Xue <Anduin@aiursoft.com>
2021-05-21 22:15:53 +0800

    Build basic unit tests for wrap project.

---
+++ tests/TestStartup.cs
@@ -0,0 +1,10 @@
+﻿using Aiursoft.Wrap;
+using Microsoft.Extensions.Configuration;
+
+namespace Aiursoft.Wrapgate.Tests
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration) { }
+    }
+}
