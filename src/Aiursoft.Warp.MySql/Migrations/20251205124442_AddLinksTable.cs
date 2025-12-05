725cd6c9428cf257af56a6102004cf56eadc0b4b
Anduin Xue <anduin@aiursoft.com>
2025-12-05 12:45:08 +0000

    feat: Add ShorterLinks table with properties and relationships in MySql migration

---
+++ src/Aiursoft.Warp.MySql/Migrations/20251205124442_AddLinksTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.Warp.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLinksTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShorterLinks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TargetUrl = table.Column<string>(type: "longtext", maxLength: 65535, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RedirectTo = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ExpireAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    IsCustom = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    IsPrivate = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    MaxClicks = table.Column<long>(type: "bigint", nullable: true),
+                    Clicks = table.Column<long>(type: "bigint", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UserId = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShorterLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShorterLinks_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShorterLinks_UserId",
+                table: "ShorterLinks",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShorterLinks");
+        }
+    }
+}
