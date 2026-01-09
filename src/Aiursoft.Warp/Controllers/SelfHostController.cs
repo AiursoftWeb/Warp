ac9e4f72a7adf9bef5da1c4fb1dac755424f7cd5
Anduin Xue <anduin@aiursoft.com>
2026-01-09 12:32:26 +0000

    Add new self host page.

---
+++ src/Aiursoft.Warp/Controllers/SelfHostController.cs
@@ -0,0 +1,30 @@
+using Aiursoft.Warp.Models.SelfHostViewModels;
+using Aiursoft.Warp.Services;
+using Aiursoft.UiStack.Navigation;
+using Aiursoft.WebTools.Attributes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace Aiursoft.Warp.Controllers;
+
+/// <summary>
+/// This controller handles the self-host deployment information page.
+/// </summary>
+[AllowAnonymous]
+[LimitPerMin]
+public class SelfHostController(IStringLocalizer<SelfHostController> localizer) : Controller
+{
+    [RenderInNavBar(
+        NavGroupName = "Self Host",
+        NavGroupOrder = 5,
+        CascadedLinksGroupName = "Self Host",
+        CascadedLinksIcon = "cloud-upload",
+        CascadedLinksOrder = 1,
+        LinkText = "Self host a new server",
+        LinkOrder = 1)]
+    public IActionResult Index()
+    {
+        return this.StackView(new IndexViewModel());
+    }
+}
---
+++ src/Aiursoft.Warp/Models/SelfHostViewModels/IndexViewModel.cs
@@ -0,0 +1,11 @@
+using Aiursoft.UiStack.Layout;
+
+namespace Aiursoft.Warp.Models.SelfHostViewModels;
+
+public class IndexViewModel : UiStackLayoutViewModel
+{
+    public IndexViewModel()
+    {
+        PageTitle = "Self Host";
+    }
+}
