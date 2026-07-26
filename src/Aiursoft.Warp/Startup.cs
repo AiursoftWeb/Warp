2d076e024750b066b76a89d47e059ad4d296c1ca
Anduin Xue <anduin@aiursoft.com>
2026-07-26 17:57:14 +0000

    Migrate to Aiursoft.UiStack 10.0.22 - embedded static assets

--- src/Aiursoft.Warp/Startup.cs
+++ src/Aiursoft.Warp/Startup.cs
@@ -7,6 +7,7 @@ using Aiursoft.Warp.InMemory;
 using Aiursoft.Warp.MySql;
 using Aiursoft.Warp.Services.Authentication;
 using Aiursoft.Warp.Sqlite;
+using Aiursoft.UiStack;
 using Aiursoft.UiStack.Layout;
 using Aiursoft.UiStack.Navigation;
 using Microsoft.AspNetCore.Mvc.Razor;
@@ -80,6 +81,7 @@ public class Startup : IWebStartup
     {
         app.UseExceptionHandler("/Error/Code500");
         app.UseStatusCodePagesWithReExecute("/Error/Code{0}");
+        app.UseUIStack();
         app.UseStaticFiles();
         app.UseRouting();
         app.UseAuthentication();
